353d118cdf32b35d491bc63f293df9a30a78cf94
jakequarmson <jake.cyfe@gmail.com>
2021-03-26 17:19:30 +0000

    Fincher questionnaire finalised

--- tasks/conditionals_take_home.py
+++ tasks/conditionals_take_home.py
@@ -1,34 +1,34 @@
+# python3.9
 # list of Fincher films
-fincher_discog = ["Se7en", "Fight Club", "The Curious Case of Benjamin Button", "The Game"]
+fincherCatalog = ["Se7en", "Fight Club", "The Curious Case of Benjamin Button", "The Game"]
 
-# questionaire on fincher discography viewing
+# questionnaire on fincher discography viewing
 question = "Have you seen"
 follow_up = "How about"
 logged = "se7en"
 
 # test for se7en
 while logged == "se7en":
-    answer1 = input(f"{question} {fincher_discog[0]}\n ")
+    answer1 = input(f"{question} {fincherCatalog[0]}\n ")
     answer1 = answer1.lower()
     if answer1 == "yes" or answer1 == "yeah":
         print("taste!")
     else:
         print("do better")
 
-
+    # test for fight club
     logged = "fight_club"
-    answer2 = input(f"{follow_up} {fincher_discog[1]} ")
+    answer2 = input(f"{follow_up} {fincherCatalog[1]}\n ")
     answer2 = answer2.lower()
     while logged == "fight_club" and logged != "no":
-        if logged == "yes" or logged == "yeah":
+        if answer2 == "yes" or logged == "yeah":
             print("you live in a society")
 
-        elif logged == "no":
-            print("pattern up")
 
         else:
-            print("you should join a fight club")
+            print("pattern up!")
 
+        # breaking loop
         logged = "end"
 
         print("bye")
