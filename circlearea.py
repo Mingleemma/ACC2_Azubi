3d8d4aaedc273f545a87915e275a2c769220a5b5
Mingleemma <mingleemma09@gmail.com>
2021-03-29 12:37:19 +0000

    functions

---
+++ circlearea.py
@@ -0,0 +1,15 @@
+# # input would be radius
+# # area = pi * r(2)
+# import math
+# pi = float(math.pi())
+
+def able():
+    print('Hello this is Able')
+
+def ability():
+    return 'Hello this has ability'
+
+able = able()
+ability = ability()
+print(str(able) + ' I am working')
+print(ability + ' and it is working')
---
+++ mod.py
@@ -0,0 +1,35 @@
+# from math import fabs, floor
+
+# abso = 4.78
+
+# made = fabs(abso)
+# made2 = floor(abso)
+# print(made)
+# print(made2)
+
+
+#NonFruitful Function
+# def demo():
+#     y = 2
+
+# def demo(a, b):
+#     y = 2 + a + b
+#     return y
+
+def demo(a, b):
+    
+    # return 2 + a + b + 2 + 6
+    sum = 2 + a + b + 2 + 6
+
+print(demo(6, 7))
+
+
+
+
+# mydemo = demo(5, 6)
+# print(mydemo)
+
+# print(demo())
+
+# sum = 5
+# sum = sum + mydemo
