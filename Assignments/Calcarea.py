d7de0f74d58e8a25cc5b4e61a225a29970d52160
kwamefosuaddai <kwamefosuaddai@gmail.com>
2021-03-29 09:32:13 -0400

    area of circle

---
+++ Assignments/Calcarea.py
@@ -0,0 +1,14 @@
+# input radius
+# area = pi * raduis**2
+import math
+r = float(input("enter radius: "))
+def area_circle(r):
+    a = math.pi * (r ** 2)
+    return a
+
+print(area_circle(r))
+
+
+
+
+
