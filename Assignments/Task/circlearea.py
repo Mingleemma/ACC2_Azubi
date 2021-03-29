0fb4dc6127b98e851ebea88aeff9f302286fc6ef
nsowah-hub <a.nsowahcobbina@gmail.com>
2021-03-29 13:02:03 +0000

    Morning Session 29th March

---
+++ Assignments/Task/circlearea.py
@@ -0,0 +1,27 @@
+import math
+
+def area_of_circle(r):
+    a = r**2 * math.pi
+    return a
+
+r = float(input("Enter the radius of the circle: "))
+print("%.2f" %area_of_circle(r))
+
+
+
+
+
+def able():
+    print('Hello this is Able')
+
+def ability():
+    return 'Hello this Ability'
+
+able = able()
+ability = ability()
+print(able)
+print(ability)
+
+
+
+
