f5736755db9d8e6a216988496c6937034585e17e
Nii Odoi <nii.odoi35@gmail.com>
2021-03-29 14:16:10 +0000

    New ATM-mimic Project

---
+++ Task/functions.py
@@ -0,0 +1,9 @@
+import math
+#input is the radius
+# area = pi * (r**2)
+
+def area(r):
+    c_area = math.pi * (r**2)
+    return c_area
+#radius = float(input('Enter radius here: '))
+print (area(10))
