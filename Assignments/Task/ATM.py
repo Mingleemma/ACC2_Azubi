0ab4357577189b2d7d7e9405884b944c2e3e477b
Samuel Boame Asiedu <53560041+asiedusam@users.noreply.github.com>
2021-04-01 15:06:04 +0000

    Cash Money ATM 2

--- Assignments/Task/ATM.py
+++ Assignments/Task/ATM.py
@@ -1,70 +1,75 @@
-users = {
-    "username": "trial",
-    "pin": 0000,
-    "balance": {
-        "GHS": 140.00,
-        "USD": 0.00
-    }
-}
-valerr = "Please type a number \n"
+import json
 
-print("Welcome to Cash Money ATM \n\n")
-while True:
-    username = input("Please type your username to log in: \n")
-    if username == users["username"]:
-        while True:
-            password = input("Please type your pin: \n")
-            try:
+with open("accounts.json") as accountsfile:
+    users = json.load(accountsfile)
+    valerr = "Please type a number \n"
+    sorry = "Sorry, you do not have enough money in this account\n\n"
 
-                if int(password) == users["pin"]:
-                    print("You have successfully logged in")
-                    while True:
-                        action = input("1: Check Balance "
-                                       "\n2: Withdraw Money \nEnter Option Here: \n3: Exit \n")
-                        try:
-                            if int(action) == 1:
-                                print("Your balance is \nGHS: {}\nUSD: {}".format(users["balance"]["GHS"],
-                                                                                  users["balance"]["USD"]))
-                            elif int(action) == 2:
-                                while True:
-                                    currency = input("Which account do you want to withdraw from?\n1: GHS\n2: USD\n")
-                                    try:
-                                        if int(currency) == 1:
-                                            amount = float(input("How much do you want to withdraw?\n"))
+    print("\n\nWelcome to Cash Money ATM \n")
+    while True:
+        username = input("Please type your username to log in: \n")
+        for user in users:
+
+            if username in user["username"]:
+                while True:
+                    pin = input("Please type your pin: \n")
+                    try:
+
+                        if int(pin) == user["pin"]:
+                            print("You have successfully logged in")
+                            while True:
+                                action = input("1: Check Balance "
+                                               "\n2: Withdraw Money \nEnter Option Here: \n3: Exit \n")
+                                try:
+                                    if int(action) == 1:
+                                        print("Your balance is \nGHS: {}\nUSD: {} \n\n".format(user["balance"]["GHS"],
+                                                                                               user["balance"]["USD"])
+                                              )
+                                    elif int(action) == 2:
+                                        while True:
+                                            currency = input(
+                                                "Which account do you want to withdraw from?\n1: GHS\n2: USD\n")
                                             try:
-                                                if amount <= users["balance"]["GHS"]:
-                                                    users["balance"]["GHS"] = round((users["balance"]
-                                                                                     ["GHS"] - amount), 2)
-                                                    print("Your balance is \nGHS: {}\nUSD: {}".format(users["balance"]
-                                                                                                      ["GHS"],
-                                                                                                      users["balance"][
-                                                                                                          "USD"]))
-                                                    break
-                                                else:
-                                                    print("Sorry, you do not have enough money in your account")
+                                                if int(currency) == 1:
+                                                    amount = float(input("How much do you want to withdraw?\n"))
+                                                    try:
+                                                        if amount <= user["balance"]["GHS"]:
+                                                            user["balance"]["GHS"] = round((user["balance"]
+                                                                                            ["GHS"] - amount), 2)
+
+                                                            print("Your new balance is \nGHS: {}\nUSD: {}\n\n".format(
+                                                                user["balance"]["GHS"],
+                                                                user["balance"]["USD"])
+                                                            )
+                                                            break
+                                                        else:
+                                                            print(sorry)
+                                                    except ValueError:
+                                                        print(valerr)
+                                                elif int(currency) == 2:
+                                                    amount = float(input("How much do you want to withdraw?\n"))
+                                                    try:
+                                                        if amount <= user["balance"]["USD"]:
+                                                            user["balance"]["USD"] = round((user["balance"]
+                                                                                            ["USD"] - amount), 2
+                                                                                           )
+
+                                                            print("Your new balance is \nGHS: {}\nUSD: {} \n\n".format(
+                                                                user["balance"]["GHS"],
+                                                                user["balance"]["USD"])
+                                                            )
+                                                            break
+                                                        else:
+                                                            print(sorry)
+                                                    except ValueError:
+                                                        print(valerr)
                                             except ValueError:
                                                 print(valerr)
-                                        elif int(currency) == 2:
-                                            amount = float(input("How much do you want to withdraw?\n"))
-                                            try:
-                                                if amount <= users["balance"]["USD"]:
-                                                    users["balance"]["GHS"] = round((users["balance"]
-                                                                                     ["USD"] - amount), 2)
-                                                    print("Your balance is \nGHS: {}\nUSD: {}".format(
-                                                        users["balance"]["GHS"],
-                                                        users["balance"]["USD"]))
-                                                    break
-                                                else:
-                                                    print("Sorry, you do not have enough money in your account")
-                                            except ValueError:
-                                                print(valerr)
-                                    except ValueError:
-                                        print(valerr)
-                            elif int(action) == 3:
-                                break
-                        except ValueError:
-                            print(valerr)
-                    break
-            except ValueError:
-                print("The pin should be a 4 digit number")
-        break
+                                    elif int(action) == 3:
+                                        break
+                                except ValueError:
+                                    print(valerr)
+                            break
+                    except ValueError:
+                        print("The pin should be a 4 digit number")
+                break
