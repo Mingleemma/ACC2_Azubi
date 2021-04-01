34020d7560d752eadedc6539a88492050988f84a
Hetty2021 <henrietta.ampofo@azubiafrica.org>
2021-04-01 16:27:22 +0000

    Afternoon session 25th March

--- Assignments/Conditionals.py
+++ Assignments/Conditionals.py
@@ -1,15 +1,36 @@
+
+userDictionary = {
+    "ADWOA" : "You were born on Monday",
+    "KOJO" : "You were born on Monday",
+    "ABENA" : "You were born on Tuesday",
+    "KWABENA" : "You were born on Tuesday",
+    "KWAKU" : "You were born on Wednesday",
+    "AKUA" : "You were born on Wednesday",
+    "YAA" : "You were born on Thursday",
+    "YAW" : "You were born on Thursday",
+    "AFIA" : "You were born on Friday",
+    "KOFI" : "You were born on Friday",
+    "AMA" : "You were born on Saturday",
+    "KWAME" : "You were born on Saturday",
+    "AKOSUA" : "You were born on Sunday",
+    "KWESI" : "You were born on Sunday"
+}
+#myFavoriteFruitDictionary["Adam"]
+
+# print(userDictionary['Abena'])
+
+
+
 response = "yes"
 while response == "yes":
     userInput = input("What is your name? ")
-    userInput = userInput.lower()
-    if userInput == "kojo" or userInput == "adwoa":
-       if userInput == "kojo":
-           print("You are a boy born on Monday")
-       elif userInput == "adwoa":
-           print("You are a girl born on Monday")
-
+    userInput = userInput.upper()
+    print(userInput)
+    a = userInput
+    if userInput in userDictionary:
+        print(userDictionary[userInput])
     else:
-         print("You were not born on Monday")
+         print("Try again")
 
     response = input("Do you want to check for another name? Answer yes/no. ")
     response = response.lower()
