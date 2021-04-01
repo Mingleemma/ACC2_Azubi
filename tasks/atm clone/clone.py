a87d327f89a94982d4afcf96e1408d3de8c9153a
jakequarmson <jake.cyfe@gmail.com>
2021-04-01 10:41:05 +0000

    Menu loop remains

--- tasks/atm clone/clone.py
+++ tasks/atm clone/clone.py
@@ -3,9 +3,9 @@
 
 # Dictionary of registered users imported from bank database
 userData = {
-    'thebe': {'username': 'thebe', 'pin': '2268', 'balanceGHS': '14_863.54', 'balanceUSD': '0.00'},
-    'mayor': {'username': 'mayor', 'pin': '8438', 'balanceGHS': '7_186.96', 'balanceUSD': '12.75'},
-    'flysiifu': {'username': 'flysiifu', 'pin': '0336', 'balanceGHS': '3_043.12', 'balanceUSD': '36.84'},
+    'thebe': {'username': 'thebe', 'pin': '2268', 'balanceGHS': 14_863.54, 'balanceUSD': 0.00},
+    'mayor': {'username': 'mayor', 'pin': '8438', 'balanceGHS': 7_186.96, 'balanceUSD': 12.75},
+    'flysiifu': {'username': 'flysiifu', 'pin': '0336', 'balanceGHS': 3_043.12, 'balanceUSD': 36.84},
     }
 
 # request for username
@@ -47,10 +47,60 @@ if action == "3":
         if statReq == "1" or statReq == "2" or statReq == "3":
             print("You would receive your statement within three business days")
             break
+        # invalid input so question asked again
         else:
             statOpt = "What period should the statement cover?"
             statOpt += "\n1 - Last 7 days \t 2 - Last 30 days \t 3 - Last 3 months\n"
             statReq = input(statOpt)
             statReq = str(statReq)
+# withdrawing funds
+if action == "2":
+    withOpt = "Which account do you wish to withdraw from?"
+    withOpt += "\n1 - Cedi Account \t 2 - Dollar Account\n"
+    withReq = input(withOpt)
+    withReq = str(withReq)
+    while withReq != "1" or withReq != "2":
+        if withReq == "1":
+            withAmt = input("What amount do you wish to withdraw?\n ")
+            withAmt = int(withAmt)
+            tmpBal = int(userData[login]['balanceGHS']) - withAmt
+            if tmpBal >= 5:
+                askRec = "Do you want a receipt for this transaction?"
+                askRec += "\n1 - Yes \t 2 - No\n"
+                confRec = input(askRec)
+                if confRec == "1":
+                    print("Please wait for your receipt.")
+                    print("Please take your cash.")
+                    userData[login]['balanceGHS'] = tmpBal
+                    break
+                else:
+                    print("Please take your cash.")
+                    userData[login]['balanceGHS'] = tmpBal
 
+            else:
+                print("Your balance is insufficient.\n Please enter a different amount.")
 
+        elif withReq == "2":
+            withAmt = input("What amount do you wish to withdraw?\n ")
+            withAmt = int(withAmt)
+            tmpBal = int(userData[login]['balanceUSD']) - withAmt
+            if tmpBal >= 2:
+                askRec = "Do you want a receipt for this transaction?"
+                askRec += "\n1 - Yes \t 2 - No\n"
+                confRec = input(askRec)
+                if confRec == "1":
+                    print("Please wait for your receipt.")
+                    print("Please take your cash.")
+                    userData[login]['balanceUSD'] = tmpBal
+                    break
+                else:
+                    print("Please take your cash.")
+                    userData[login]['balanceUSD'] = tmpBal
+            else:
+                print("Your balance is insufficient.\n Please enter a different amount.")
+
+        else:
+            withOpt = "Which account do you wish to withdraw from?"
+            withOpt += "\n1 - Cedi Account \t 2 - Dollar Account\n"
+            withReq = input(withOpt)
+            withReq = str(withReq)
