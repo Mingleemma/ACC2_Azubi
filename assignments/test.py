653768e59d7265d7e57d58d72a6918a398146e0b
Oyaps <oyapiasamoah95@gmail.com>
2021-04-12 08:29:15 +0800

    Practice time

---
+++ assignments/test.py
@@ -0,0 +1,13 @@
+# Area of a circle
+
+def area(r):
+    a = 3.14 * (r**2)
+    return a
+print(area(8))
+
+# Area of a circle with math.pi
+import math
+def areaCircle(r):
+    a = math.pi * (r**2)
+    return a
+print(area(8))
