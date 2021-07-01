ee79bd877befc9e777f9191503dd44adff3603b0
kwamefosuaddai <80292254+kwamefosuaddai@users.noreply.github.com>
2021-07-01 15:27:59 +0000

    Test_project

---
+++ Downloads/Dairy/dfsms/includes/footer.php
@@ -0,0 +1,9 @@
+            <div class="hk-footer-wrap container">
+                <footer class="footer">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-12">
+<p>Developed By<a href="https://phpgurukul.com/" class="text-dark">PHPGurukul</a> ©2019</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
